e2bb7539ad0c938ce5a78d714fd22b485c34c2e3
denis <denis.shybeko@gmail.com>
2018-10-20 09:39:28 +0000

    fix server.js

--- server.js
+++ server.js
@@ -3,33 +3,30 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const app = express();
-const server = http.createServer().listen(process.env.PORT, process.env.IP);
-const io = require('socket.io');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
-//app.set('port', 8050);
-app.use('/public', express.static(__dirname + '/public'));
-app.use('/shared', express.static(__dirname + '/shared'));
-
-// Routing
-app.get('/', function (request, response) {
-    response.sendFile(path.join(__dirname, 'index.html'));
+//app.set('port', 8080);
+app.get('/server', function (request, response) {
+    response.sendFile(path.join(__dirname, 'server/index.html'));
 });
 
-app.get('/login', function (request, response) {
+app.get('/', function (request, response) {
     response.sendFile(path.join(__dirname, 'client/index.html'));
 });
 
-var socket = io.listen(server);
+app.use('/client', express.static(__dirname + '/client'));
+app.use('/server', express.static(__dirname + '/server'));
 
 // Starts the server
-// server.listen(8050, function () {
-//     console.log('Starting server on port 8050');
-// });
+server.listen(8080, function () {
+    console.log('Starting server on port 8050');
+});
 
 const players = {};
 
 // Add the WebSocket handlers
-socket.sockets.on('connection', function (socket) {
+io.on('connection', function (socket) {
 
     console.log('Connected!');
     socket.on('new player', function () {
